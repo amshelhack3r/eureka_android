5070135ca13343c5cfc5747ffa9ccb7ceaedd49e
hack3r <amshelhack3r@gmail.com>
2018-09-27 12:02:43 +0300

    test sqlite importer

--- app/src/main/java/com/hack3r/amshel/eurekawaters/MainActivity.java
+++ app/src/main/java/com/hack3r/amshel/eurekawaters/MainActivity.java
@@ -6,8 +6,15 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.ajts.androidmads.sqliteimpex.SQLiteImporterExporter;
+import com.hack3r.amshel.eurekawaters.activity.MessagingActivity;
+import com.hack3r.amshel.eurekawaters.activity.ReadingActivity;
+import com.hack3r.amshel.eurekawaters.activity.SmsActivity;
+import com.hack3r.amshel.eurekawaters.library.Mutall;
+
 public class MainActivity extends AppCompatActivity {
     Mutall mutall;
+    SQLiteImporterExporter sqLiteImporterExporter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,12 +23,12 @@ public class MainActivity extends AppCompatActivity {
         mutall = new Mutall(this);
     }
     public void monitorMessages(View view){
-        Intent i = new Intent(MainActivity.this, Accounts.class);
+        Intent i = new Intent(MainActivity.this, MessagingActivity.class);
         startActivity(i);
     }
 
     public void smsPage(View view){
-        Intent intent = new Intent(this, SmsPage.class);
+        Intent intent = new Intent(this, SmsActivity.class);
         startActivity(intent);
     }
 
@@ -29,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
 //        String message = "Hold up lad, Next time it will be ready";
 //        mutall.showToast(message, "warning");
-        Intent intent =new Intent(this, ReadingPage.class);
+        Intent intent =new Intent(this, ReadingActivity.class);
         startActivity(intent);
     }
 
