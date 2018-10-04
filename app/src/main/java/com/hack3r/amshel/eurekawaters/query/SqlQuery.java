0095f703f45aee3999e1821ccb5b9e963b650063
hack3r <amshelhack3r@gmail.com>
2018-10-04 14:29:26 +0300

    Add a method which breaks a querystring and formulates an sql based on the querystring

--- app/src/main/java/com/hack3r/amshel/eurekawaters/query/SqlQuery.java
+++ app/src/main/java/com/hack3r/amshel/eurekawaters/query/SqlQuery.java
@@ -18,7 +18,7 @@ public class SqlQuery {
 
     public static final String subq = "subq";
     public static final String subq1 = "subq1";
-
+//query to create the table reading
      public static final String CREATE_READING_TABLE = "CREATE TABLE "
             +TABLE_READING +"("
             +READING_COLUMN_ID +" INTEGER PRIMARY KEY AUTOINCREMENT, "
@@ -26,7 +26,7 @@ public class SqlQuery {
             +READING_COLUMN_DATE +" TEXT NOT NULL, "
             +READING_COLUMN_VALUE+" INTEGER NOT NULL, " +
              "FOREIGN KEY("+READING_COLUMN_CLIENT+") REFERENCES "+ TABLE_CLIENT +"("+CLIENT_COLUMN_ID +"))";
-
+//query to create the table client
     public static final String CREATE_CLIENT_TABLE = "CREATE TABLE "
             +TABLE_CLIENT +"("
             +CLIENT_COLUMN_ID +" INTEGER PRIMARY KEY AUTOINCREMENT, "
@@ -35,11 +35,11 @@ public class SqlQuery {
             +CLIENT_COLUMN_METER+" TEXT, "
             +CLIENT_COLUMN_TELEPHONE+ " TEXT) ";
 
-
+//query to drop both the client and reading table
     public static final String DROP_TABLE_CLIENT ="DROP TABLE IF EXISTS "+TABLE_CLIENT;
     public static final String DROP_TABLE_READING ="DROP TABLE IF EXISTS "+TABLE_READING;
 
-//    insert a reading
+    //insert a single reading
     public static final String INSERT_READING = "INSERT into "
                                                     +TABLE_READING
                                                     +"(" +READING_COLUMN_DATE+ ", "
@@ -48,7 +48,7 @@ public class SqlQuery {
                                                     +"VALUES(?, ?, (SELECT "+CLIENT_COLUMN_ID
                                                     + " FROM " +TABLE_CLIENT
                                                     + " WHERE "+CLIENT_COLUMN_CODE+"=?))";
-
+    //query to insert a new client
     public static final String INSERT_CLIENTS = "INSERT into "
                                                     +TABLE_CLIENT
                                                     +"("+CLIENT_COLUMN_CODE+", "
@@ -86,15 +86,22 @@ public class SqlQuery {
                                                             TABLE_CLIENT+" LEFT JOIN ("+MAX_READING+") AS "+subq1+
                                                         " ON "+TABLE_CLIENT+"."+CLIENT_COLUMN_ID+"="+subq1+"."+CLIENT_COLUMN_ID+
                                                         " WHERE "+CLIENT_COLUMN_CODE +"=?";
-
+    /**
+     * Query for selecting the edge cases.
+     * What are edge cases???
+     * These are records in which dates dont have the yyyy-mm-dd format
+     * These dates have dd-mm-yyyy format and since elias had collected quite afew i didnt change the
+     * format of the date until the period was over
+     */
     public static final String DAILY_READING_POSTING = "SELECT "
                                                             +TABLE_CLIENT+".*, "
                                                             +TABLE_READING+".* "
                                                         +" FROM "
                                                             +TABLE_READING+ " INNER JOIN "+ TABLE_CLIENT +" ON "
                                                             +TABLE_READING+"."+CLIENT_COLUMN_ID+"=" +TABLE_CLIENT+"."+CLIENT_COLUMN_ID
-                                                        +" WHERE "+TABLE_READING+"."+READING_COLUMN_DATE+ " NOT LIKE ?";
+                                                        +" WHERE "+TABLE_READING+"."+READING_COLUMN_DATE+ " NOT LIKE '2018-%'";
 
+    //query for getting all readings in the database
     public static final String ALL_READINGS = "SELECT "
                                                     +TABLE_CLIENT+".*, "
                                                     +TABLE_READING+".* "
@@ -104,7 +111,29 @@ public class SqlQuery {
                                                 + " ORDER BY "+READING_COLUMN_DATE+ " DESC";
 
 
+    //function that accepts a query string and formulates an sql based on it
+    public static String formulateSql(String query){
 
+        //split the query
+        String[] params = query.split("[= > <]+");
+
+        String column = params[0];
+        String val = "'"+params[1]+"'";
+
+        //query for fetiching readings of a particular criteria
+        String SPECIFIC_READINGS = "SELECT "
+                +TABLE_CLIENT+".*, "
+                +TABLE_READING+".* "
+                +" FROM "
+                +TABLE_READING+ " INNER JOIN "+ TABLE_CLIENT +" ON "
+                +TABLE_READING+"."+CLIENT_COLUMN_ID+"=" +TABLE_CLIENT+"."+CLIENT_COLUMN_ID
+                +" WHERE "+column +"=" +val
+                + " ORDER BY "+READING_COLUMN_DATE+ " DESC";
+
+        return SPECIFIC_READINGS;
+    }
+
+    //set unique indexes
     public static final String READING_INDEXES = "CREATE UNIQUE INDEX "+INDEX1 +" ON "+TABLE_READING+"("+READING_COLUMN_DATE+", "+READING_COLUMN_CLIENT+ ")";
     public static final String CLIENT_INDEXES = "CREATE UNIQUE INDEX "+INDEX2 +" ON "+TABLE_CLIENT+"("+CLIENT_COLUMN_CODE+")";
 
