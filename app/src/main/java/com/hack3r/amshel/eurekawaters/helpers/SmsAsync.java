c4f496306ab87b5aa27ff0babd0e66492cd211e4
amshelhack3r <amshelhack3r@gmail.com>
2018-11-28 14:09:56 +0300

    catch null pointer exceptions

--- app/src/main/java/com/hack3r/amshel/eurekawaters/helpers/SmsAsync.java
+++ app/src/main/java/com/hack3r/amshel/eurekawaters/helpers/SmsAsync.java
@@ -1,22 +1,25 @@
 package com.hack3r.amshel.eurekawaters.helpers;
 
-import android.app.job.JobService;
+import android.app.Activity;
 import android.telephony.SmsManager;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.hack3r.amshel.eurekawaters.library.Mutall;
 import com.hack3r.amshel.eurekawaters.library.VolleyController;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Timer;
+import java.util.ArrayList;
 
-public class SmsAsync implements Runnable {
-    String url = "http:mutall.co.ke";
+public class SmsAsync extends Thread implements Runnable {
+    String TAG = SmsAsync.class.getSimpleName();
+    String url = "http://mutall.co.ke/eureka_android/client_details.php";
     @Override
     public void run() {
         getMessages();
@@ -36,33 +39,39 @@ public class SmsAsync implements Runnable {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                error.printStackTrace();
             }
         });
 
         VolleyController.getInstance().addRequestQueue(arrayRequest);
     }
 
+    /**
+     * create a method for sending sms to each of the clients
+     * @param array
+     */
     void sendMessages(JSONArray array){
         SmsManager manager = SmsManager.getDefault();
-        for (int i = 0; i>array.length(); i++ ){
+        for (int i = 0; i<array.length(); i++ ){
             try {
                 JSONObject object = array.getJSONObject(i);
-                manager.sendTextMessage(object.getString("number"), null, object.getString("message"), null, null);
-                delay();
+
+                ArrayList<String> parts = manager.divideMessage(object.getString("message"));
+
+                String phone_number = object.getString("number");
+
+                manager.sendMultipartTextMessage(phone_number, null, parts, null, null);
+
+
+                this.sleep(7000);
             }catch (JSONException error){
+                error.printStackTrace();
+            }catch (InterruptedException error){
+                error.printStackTrace();
+            }catch (NullPointerException error){
                 error.printStackTrace();
             }
         }
-
-
-    }
-    /**
-     * function for delyimg 1000 seconds
-     */
-    void delay() {
-        Timer timer = new Timer();
-
     }
 }
 
